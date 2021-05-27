a3aa347dd291d865012edbf5c0c367967aa4e29a
Idail Ferreira <neto_br_8@hotmail.com>
2021-05-27 19:46:02 -0400

    Terceiro, novos codigos acrescentados ao program.cs pelo dono do repositorio.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,17 @@ namespace ConsoleApp1
         {
             Console.WriteLine("ola 1");
             Console.WriteLine("ola 2");
+
+            //variaveis e metodo criados a partir do dono do repositorio
+            int primeiro_numero = 10,segundo_numero = 10,valor_final;
+            public void soma()
+            {
+                valor_final = primeiro_numero + segundo_numero;
+                Console.WriteLine(valor_final);
+            }
+
+            Console.WriteLine("Pressione alguma tecla para sair.");
+            Console.ReadKey();
         }
     }
 }
